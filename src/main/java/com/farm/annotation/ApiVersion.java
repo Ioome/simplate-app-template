7d19639d09dfcb163628e229e97a74711dc26235
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-28 10:24:01 +0800

    sutton
    版本接口增加控制

--- src/main/java/com/farm/annotation/ApiVersion.java
+++ src/main/java/com/farm/annotation/ApiVersion.java
@@ -1,16 +1,19 @@
 package com.farm.annotation;
 
-/**
- * @name: ApiVersion
- * @author: sutton
- * @date: 2023-04-26 17:09
- * @description: ApiVersion
- */
+
 
 import org.springframework.web.bind.annotation.Mapping;
 
 import java.lang.annotation.*;
 
+
+/**
+ * @name: ApiVersion
+ * @description: <a href="http://localhost:8080/api/v1/user/get">...</a> <a href="http://localhost:8080/api/v1.1/user/get">...</a>
+ * @date: 2023-04-26 15:50
+ * @version: 1.0
+ * @since JDK 1.8
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
